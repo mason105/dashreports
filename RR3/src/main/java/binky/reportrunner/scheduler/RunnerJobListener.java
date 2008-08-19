5982435c6f5c500193c737ee15a5bdd413a2e2f5
danielgrout <danielgrout@29642f0a-3d76-11de-b8bc-ddcf2599578a>
2008-08-19 20:34:36 +0000

    I'm going commit mad!!
    Just done the listener that writes events to the event table and also injects the job/dao into the context prior to execution

--- RR3/src/main/java/binky/reportrunner/scheduler/RunnerJobListener.java
+++ RR3/src/main/java/binky/reportrunner/scheduler/RunnerJobListener.java
@@ -1,17 +1,21 @@
 package binky.reportrunner.scheduler;
 
+import java.util.Calendar;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.JobListener;
 
 import binky.reportrunner.dao.RunnerHistoryDao;
 import binky.reportrunner.dao.RunnerJobDao;
+import binky.reportrunner.data.RunnerHistoryEvent;
+import binky.reportrunner.data.RunnerJob;
 
 public class RunnerJobListener implements JobListener {
 
 	private RunnerHistoryDao runnerHistoryDao;
 	private RunnerJobDao runnerJobDao;
-	
+
 	public RunnerJobDao getRunnerJobDao() {
 		return runnerJobDao;
 	}
@@ -24,18 +28,36 @@ public class RunnerJobListener implements JobListener {
 		return "ReportRunnerCoreJobListener";
 	}
 
-	public void jobExecutionVetoed(JobExecutionContext arg0) {
-		// TODO Auto-generated method stub
+	public void jobExecutionVetoed(JobExecutionContext ctx) {
+		RunnerHistoryEvent event = new RunnerHistoryEvent();
+		event.setGroupName(ctx.getJobDetail().getGroup());
+		event.setJobName(ctx.getJobDetail().getName());
+		event.setMessage("Job Execution Vetoed");
+		event.setTimestamp(Calendar.getInstance().getTime());
+
+		runnerHistoryDao.saveEvent(event);
 	}
 
-	public void jobToBeExecuted(JobExecutionContext arg0) {
-		// TODO Auto-generated method stub
+	public void jobToBeExecuted(JobExecutionContext ctx) {
+		String jobName = ctx.getJobDetail().getName();
+		String groupName = ctx.getJobDetail().getGroup();
+
+		RunnerJob job = runnerJobDao.getJob(jobName, groupName);
+		ctx.put("runnerJob", job);
 
 	}
 
-	public void jobWasExecuted(JobExecutionContext arg0,
-			JobExecutionException arg1) {
-		// TODO Auto-generated method stub
+	public void jobWasExecuted(JobExecutionContext ctx, JobExecutionException ex) {
+		Boolean success = ex == null;
+
+		RunnerHistoryEvent event = new RunnerHistoryEvent();
+		event.setGroupName(ctx.getJobDetail().getGroup());
+		event.setJobName(ctx.getJobDetail().getName());
+		event.setMessage(success ? "Job Execution Success"
+				: "Job Execution Failure: " + ex.getMessage());
+		event.setTimestamp(Calendar.getInstance().getTime());
+
+		runnerHistoryDao.saveEvent(event);
 	}
 
 	public RunnerHistoryDao getRunnerHistoryDao() {

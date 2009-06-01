581d943d44d5d6c706a375370c472bdf385cebba
dangrout <dangrout@29642f0a-3d76-11de-b8bc-ddcf2599578a>
2009-06-01 23:24:40 +0000

    Added licence-ing and Copyright information into the files - may I recommend this wonderful Eclipse plugin for doing such things: http://www.wdev91.com/
    Fantastic!

--- src/main/java/binky/reportrunner/engine/renderers/exporters/XLSExporter.java
+++ src/main/java/binky/reportrunner/engine/renderers/exporters/XLSExporter.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Daniel Grout.
+ * 
+ * GNU GENERAL PUBLIC LICENSE - Version 3
+ * 
+ * This file is part of Report Runner (http://code.google.com/p/reportrunner).
+ * 
+ * Report Runner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Report Runner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Report Runner. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Module: XLSExporter.java
+ ******************************************************************************/
 package binky.reportrunner.engine.renderers.exporters;
 
 import java.io.IOException;

abc70d1ad4f58c519e28727e08e008416d6b2760
dangrout@gmail.com <dangrout@gmail.com@29642f0a-3d76-11de-b8bc-ddcf2599578a>
2012-01-22 20:05:38 +0000

    got the grid working on the report viewer
    very hacky - need to get the source for the s2 plugin for jquery and look to do a from array data thing for it based on what i've done here

--- src/main/java/binky/reportrunner/ui/actions/job/viewer/ViewJobOutputAction.java
+++ src/main/java/binky/reportrunner/ui/actions/job/viewer/ViewJobOutputAction.java
@@ -23,8 +23,16 @@
 package binky.reportrunner.ui.actions.job.viewer;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.beanutils.DynaBean;
+import org.apache.commons.beanutils.DynaProperty;
+import org.apache.commons.beanutils.RowSetDynaClass;
+import org.apache.log4j.Logger;
 
 import binky.reportrunner.dao.ReportRunnerDao;
 import binky.reportrunner.data.RunnerHistoryEvent;
@@ -41,58 +49,133 @@ public class ViewJobOutputAction extends StandardRunnerAction {
 
 	private String jobName;
 
-
-
-	private Map<String, ViewerResults> results;
-	
-	private ReportRunnerDao<RunnerHistoryEvent,Long> historyDao;
+	private Map<String, ViewerResults> downloadResults;
+	private Map<String, List<String>> columns;
+	private ReportRunnerDao<RunnerHistoryEvent, Long> historyDao;
 	private List<RunnerJobParameter> parameters;
-	
+	private Map<String,List<Map<String, Object>>> gridResults;
+
 	private RunnerJobService jobService;
-	//private static final Logger logger = Logger.getLogger(ViewJobOutputAction.class);
+
+	 private static final Logger logger = Logger.getLogger(ViewJobOutputAction.class);
 	@Override
 	public String execute() throws Exception {
+
+		if (!super.doesUserHaveGroup(groupName))
+			throw new SecurityException(
+					"User does not have permissions for group: " + groupName);
 		
-		if (!super.doesUserHaveGroup(groupName)) throw new SecurityException("User does not have permissions for group: " + groupName);
+
 		
-		long startTime = (new Date()).getTime();
-		//TODO: NASTY HACK ALERT
-		if ((this.parameters != null) && (this.parameters.size() > 0)) {
-			RunnerJob job = jobService.getJob(jobName, groupName);
-			
-			List<RunnerJobParameter> jobParameters = job.getParameters();
-			
-			for (RunnerJobParameter p: this.parameters) {								
-				
-				for (RunnerJobParameter jp:jobParameters) {
-					if (jp.getPk().getParameterIdx().equals(p.getPk().getParameterIdx())) {
-						jp.setParameterValue(p.getParameterValue());
-						break;
+		if (jobService.getJob(jobName, groupName)!=null && jobService.getJob(jobName, groupName).getTemplateType()
+				.equals(RunnerJob.Template.NONE)) {
+			logger.debug("populating grid");
+			Map<String,RowSetDynaClass> dynaSets;
+		
+			if ((this.parameters != null) && (this.parameters.size() > 0)) {
+				logger.debug("using parameters");
+				RunnerJob job = jobService.getJob(jobName, groupName);
+
+				List<RunnerJobParameter> jobParameters = job.getParameters();
+
+				for (RunnerJobParameter p : this.parameters) {
+
+					for (RunnerJobParameter jp : jobParameters) {
+						if (jp.getPk().getParameterIdx()
+								.equals(p.getPk().getParameterIdx())) {
+							jp.setParameterValue(p.getParameterValue());
+							break;
+						}
 					}
 				}
-				
+				dynaSets = jobService.getResultSet(groupName, jobName,
+						parameters);
+
+			} else {
+				logger.debug("not using parameters");
+				dynaSets = jobService.getResultSet(groupName, jobName);
 			}
-					
-			results = jobService.getResultsForJob(jobName, groupName,
-					jobParameters);
+
+
+			this.gridResults= new LinkedHashMap<String, List<Map<String,Object>>>();			
+			
+			this.columns = new HashMap<String, List<String>>();
+			
+			for (String key : dynaSets.keySet()) {
+				List<Map<String,Object>> rows = new LinkedList<Map<String,Object>>();
+				logger.debug("getting columns for result:" + key);
+				// populate columns
+				List<String> cols = new LinkedList<String>();
+				RowSetDynaClass rsdc = dynaSets.get(key);				
+				for (DynaProperty d : rsdc.getDynaProperties()) {
+					logger.debug("got column: " + d.getName());
+					cols.add(d.getName());
+				}
+				columns.put(key, cols);
+				
+				
+				//populate data
+				for (Object r:dynaSets.get(key).getRows()) {
+					Map<String, Object> row=new LinkedHashMap<String, Object>();
+					DynaBean b = (DynaBean) r;
+					for (String c:cols)  {
+						Object v = b.get(c);
+						row.put(c, v);
+					}
+					rows.add(row);
+				}
+				this.gridResults.put(key, rows);
+			}
+			
+
+			logger.debug("redirecting to grid");
+			return "GRID";			
+		
 		} else {
-			results = jobService.getResultsForJob(jobName, groupName);
+			logger.debug("doing file download");
+			long startTime = (new Date()).getTime();
+			// TODO: NASTY HACK ALERT
+			if ((this.parameters != null) && (this.parameters.size() > 0)) {
+				RunnerJob job = jobService.getJob(jobName, groupName);
+
+				List<RunnerJobParameter> jobParameters = job.getParameters();
+
+				for (RunnerJobParameter p : this.parameters) {
+
+					for (RunnerJobParameter jp : jobParameters) {
+						if (jp.getPk().getParameterIdx()
+								.equals(p.getPk().getParameterIdx())) {
+							jp.setParameterValue(p.getParameterValue());
+							break;
+						}
+					}
+
+				}
+
+				downloadResults = jobService.getResultsForJob(jobName,
+						groupName, jobParameters);
+			} else {
+				downloadResults = jobService.getResultsForJob(jobName,
+						groupName);
+			}
+
+			long endTime = (new Date()).getTime();
+
+			// create an event for this so we can track performance of bad
+			// queries
+			RunnerHistoryEvent event = new RunnerHistoryEvent();
+			event.setGroupName(groupName);
+			event.setJobName(jobName);
+			String message = "User:" + super.getSessionUser().getUserName()
+					+ " ran job viewer";
+			event.setMessage(message);
+			event.setRunTime(endTime - startTime);
+			event.setTimestamp(new Date());
+			event.setSuccess(true);
+			historyDao.saveOrUpdate(event);
+
+			return "DOWNLOAD";
 		}
-		
-		long endTime = (new Date()).getTime();
-		
-		//create an event for this so we can track performance of bad queries
-		RunnerHistoryEvent event = new RunnerHistoryEvent();
-		event.setGroupName(groupName);
-		event.setJobName(jobName);
-		String message = "User:" + super.getSessionUser().getUserName() + " ran job viewer";
-		event.setMessage(message);
-		event.setRunTime(endTime-startTime);
-		event.setTimestamp(new Date());
-		event.setSuccess(true);
-		historyDao.saveOrUpdate(event);
-		
-		return SUCCESS;
 	}
 
 	public RunnerJobService getJobService() {
@@ -119,18 +202,42 @@ public class ViewJobOutputAction extends StandardRunnerAction {
 		this.parameters = parameters;
 	}
 
-	public Map<String, ViewerResults> getResults() {
-		return results;
-	}
-
-	public void setResults(Map<String, ViewerResults> results) {
-		this.results = results;
-	}
-
-	public void setHistoryDao(ReportRunnerDao<RunnerHistoryEvent,Long> historyDao) {
+	public void setHistoryDao(
+			ReportRunnerDao<RunnerHistoryEvent, Long> historyDao) {
 		this.historyDao = historyDao;
 	}
 
+	public Map<String, ViewerResults> getDownloadResults() {
+		return downloadResults;
+	}
 
+	public void setDownloadResults(Map<String, ViewerResults> downloadResults) {
+		this.downloadResults = downloadResults;
+	}
+
+	public Map<String, List<String>> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(Map<String, List<String>> columns) {
+		this.columns = columns;
+	}
+
+
+	public Map<String, List<Map<String, Object>>> getGridResults() {
+		return gridResults;
+	}
+
+	public void setGridResults(Map<String, List<Map<String, Object>>> gridResults) {
+		this.gridResults = gridResults;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public ReportRunnerDao<RunnerHistoryEvent, Long> getHistoryDao() {
+		return historyDao;
+	}
 
 }
--- src/main/webapp/jsp/main/viewJobGrid.jsp
+++ src/main/webapp/jsp/main/viewJobGrid.jsp
@@ -1,46 +1,74 @@
 <%@ taglib prefix="s" uri="/struts-tags"%>
+
 <%@ taglib prefix="sj" uri="/struts-jquery-tags"%>
-<%@ taglib prefix="sjg" uri="/struts-jquery-grid-tags"%>
 <html>
 <head>
-	<sj:head locale="en" jqueryui="true" jquerytheme="smoothness"/>
 
-<script language="javascript">
+<sj:head/>
+
+
+<link rel="stylesheet" type="text/css" media="screen" href="<s:url value="/js/jqGrid/css/ui.jqgrid.css"/>" />
+
+
+<script src="<s:url value="/js/jqGrid/js/i18n/grid.locale-en.js"/>" type="text/javascript"></script>
+<script src="<s:url value="/js/jqGrid/js/jquery.jqGrid.min.js"/>" type="text/javascript"></script>
+
+<script type='text/javascript'>
+
+	
 
 	function populateGrids() {
 	
-	<s:iterator value="results" status="rowstatus">
-	
-		var mydata<s:property value="rowstatus"/> = [ 
-		<s:iterator value="value.rows" var="rowItem">
-			<s:if test="rowstatus>0">,</s:if>{
-			<s:iterator value="columns.get(key)" var="colName">
-				<s:property value="colName"/>:"<s:property value="rowItem.get(colName)"/>"
-			</s:iterator>
+	<s:iterator value="gridResults"  status="rowstatusTab">
+		var mydata<s:property value="#rowstatusTab.index"/> = [ 
+		<s:iterator value="value" var="row"  status="rowstatusRow">
+			<s:if test="#rowstatusRow.index>0">,</s:if>{			
+				<s:iterator value="row" status="rowStatusCol">
+				<s:if test="#rowStatusCol.index>0">,</s:if>
+					<s:property value="key"/>:"<s:property value="value"/>"
+				</s:iterator>
 		}
 		</s:iterator> 	
 		 ]; 
 
-		for(var i=0;i<=mydata.length;i++) jQuery("#grid<s:property value="rowstatus"/>").jqGrid('addRowData',i+1,mydata<s:property value="rowstatus"/>[i]);
-	
+		for(var i=0;i<=mydata<s:property value="#rowstatusTab.index"/>.length;i++) jQuery('#mygrid<s:property value="#rowstatusTab.index"/>').jqGrid('addRowData',i+1,mydata<s:property value="#rowstatusTab.index"/>[i]);	
 	</s:iterator>
-	
 	}
 
 </script>
 </head>
-<body onload="populateGrids();">
-<sj:tabbedpanel id="grids">
-	<s:iterator value="results" status="rowstatus">
-		<sj:tab id="gridTab%{rowstatus}" target="gridDiv%{rowstatus}"/>
-		<div id="gridDiv<s:property value="rowstatus"/>">
-			<sjg:grid id="#grid%{rowstatus}" dataType="local" caption="%{jobName}" gridModel="">
-				<s:iterator value="columns" var="colName">
-					<sjg:gridColumn name="%{colName}" index="%{colName}" title="%{colName}" sortable="false"/>
-				</s:iterator>    	
-			</sjg:grid>
-    	</div>
+<body >
+	<s:iterator value="columns" status="rowstatus">
+
+    	
+    	<table  id="mygrid<s:property value="#rowstatus.index"/>" class="scroll" ></table>
+    	<div id="mygridpager<s:property value="#rowstatus.index"/>"></div>
+    	<script type='text/javascript'>
+    	
+    			jQuery("#mygrid<s:property value="#rowstatus.index"/>").jqGrid({ datatype: "local", 
+    				
+    				colNames:[
+					<s:iterator value="value" var="colName" status="rowstatusCol">	
+					<s:if test="#rowstatusCol.index>0">,</s:if>
+					'<s:property value="colName"/>'
+    				</s:iterator>
+    				], 
+    				colModel:[ 
+					<s:iterator value="value" var="colName" status="rowstatusCol">	
+					<s:if test="#rowstatusCol.index>0">,</s:if>
+    				{name:'<s:property value="colName"/>',index:'<s:property value="colName"/>'}  
+    				</s:iterator>
+    				]
+    			, multiselect: false,rowList:[10,20,30],pager: '#mygridpager<s:property value="#rowstatus.index"/>',viewrecords: true
+    			,autowidth:true
+    			,height:500	
+    			,caption: "<s:property value="key"/>"}) ;
+    	    
+    			jQuery("#mygrid<s:property value="#rowstatus.index"/>").jqGrid('navGrid','#mygridpager<s:property value="#rowstatus.index"/>',{edit:false,add:false,del:false});
+    	</script>
 	</s:iterator>
-</sj:tabbedpanel>
+	<script type="text/javascript">
+		populateGrids();
+	</script>
 </body>
 </html>
